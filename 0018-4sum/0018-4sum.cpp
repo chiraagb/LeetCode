97870f545660c30bf51b67799f06f3ca76af0672
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-03-31 14:39:09 +0530

    Time: 57 ms (82.59%), Space: 13.2 MB (88.89%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,33 +2,52 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> r;
+        
+        if(nums.empty()) return r;
         int n = nums.size();
         sort(nums.begin(),nums.end());
         
-        set<vector<int>>s;
-        
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    long long more =(long long)target - (long long)nums[i]-(long long)nums[j]-(long long)nums[k];
-                    if(binary_search(nums.begin()+k+1,nums.end(),more)){
-                        vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[k]);
-                        temp.push_back(more);
+                int left = j+1;
+                int right = n-1;
+                
+                // target = 9
+                // a[]={1,1,1,2,2,3,3,4,4,4}
+                //      i j l             r
+                // remaining = 7
+                // 1+4 = 5 < remaining
+                long long rem = (long long)target - (long long)(nums[i]+nums[j]);
+                
+                while(left<right){
+                    int two_sum = nums[left]+nums[right];
+                    if(two_sum == rem){
+                        vector<int> temp(4,0);
+                        temp[0] = nums[i];
+                        temp[1] = nums[j];
+                        temp[2] = nums[left];
+                        temp[3] = nums[right];
                         
-                        sort(temp.begin(),temp.end());
-                        s.insert(temp);
+                        r.push_back(temp);
+                        while(left<right && nums[left]==temp[2]) left++;
+                        while(left<right && nums[right]==temp[3]) right--;
+                    }
+                    else if(two_sum<rem){
+                        left++;
+                    }
+                    else{
+                        right--;
                     }
                     
-                    
                 }
+                
+                while(j+1<n && nums[j+1]==nums[j]) ++j;
+                
             }
+            while(i+1<n && nums[i+1]==nums[i]) ++i;
         }
-        for(auto it:s){
-            r.push_back(it);
-        }
+        
+        
         return r;
     }
 };
