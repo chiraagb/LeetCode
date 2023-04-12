9481606b80619963190de90d384415097f3bed8d
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-04-12 19:00:22 +0530

    Time: 3 ms (97.10%), Space: 11.8 MB (7.77%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,35 +11,24 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL) return NULL;
-        if(head->next == NULL) return head;
+        // Optimal O(n) || O(1)
+        if(head == NULL || head->next==NULL || k==0) return head;
         
         ListNode *curr = head;
-        ListNode *pre = NULL;
-        
-        int count = 1;
-        
+        int len =1;
         while(curr->next!=NULL){
-            count++;
-            pre = curr;
+            len++;
+            curr=curr->next;
+        }
+        curr->next=head;
+        k = k%len;
+        k = len-k;
+        while(k){
             curr = curr->next;
-        }
-        
-        if(k>count){
-            k=k%count;
-        }
-        while(k>0){
-            curr =head;
-            pre = NULL;
-            while(curr->next!=NULL){
-                pre = curr;
-                curr=curr->next;
-            }
-            curr->next = head;
-            pre->next = NULL;
-            head = curr;
             k--;
         }
+        head = curr->next;
+        curr->next = NULL;
         return head;
     }
 };
